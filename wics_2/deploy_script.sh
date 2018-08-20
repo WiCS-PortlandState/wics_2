2b4bfcdc657148ee398dd628980767ae6131098a
Karis Sponsler <karis.sponsler@gmail.com>
2018-08-20 10:57:18 -0700

    updating branding pictures + alyssa's profile pic

--- wics_2/deploy_script.sh
+++ wics_2/deploy_script.sh
@@ -2,5 +2,5 @@
 
 git pull origin master >> deploy_log.txt
 pkill gunicorn
-( cd .. ; gunicorn --paste production.ini 2> logs.txt )
+( cd .. ; gunicorn --paste production.ini 2> logs.txt &; disown)
 service nginx restart
